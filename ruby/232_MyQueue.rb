6b4bc5dce2dbef74acbe203a726631a66c0663ee
Paul van Duyn <paul.vanduyn@gmail.com>
2018-12-12 17:36:11 -0500

    modify p232

--- ruby/232_MyQueue.rb
+++ ruby/232_MyQueue.rb
@@ -1,56 +1,33 @@
 class MyQueue
 
-=begin
-    Initialize your data structure here.
-=end
-    def initialize()
-        @arr = []
+  def initialize()
+    @arr_in = []
+    @arr_out = []
+  end
+
+  def push(x)
+    @arr_in << x
+    x
+  end
+
+  def pop
+    if @arr_out.empty?
+      @arr_in.length.times { @arr_out.push(@arr_in.pop) }
     end
 
+    @arr_out.pop
+  end
 
-=begin
-    Push element x to the back of queue.
-    :type x: Integer
-    :rtype: Void
-=end
-    def push(x)
-        @arr << x
-        x
+  def peek
+    if @arr_out.empty?
+      @arr_in.length.times { @arr_out.push(@arr_in.pop) }
     end
 
+    @arr_out[-1]
+  end
 
-=begin
-    Removes the element from in front of queue and returns that element.
-    :rtype: Integer
-=end
-    def pop()
-        @arr.shift
-    end
-
-x
-=begin
-    Get the front element.
-    :rtype: Integer
-=end
-    def peek()
-        @arr[0]
-    end
-
-
-=begin
-    Returns whether the queue is empty.
-    :rtype: Boolean
-=end
-    def empty()
-        @arr.length == 0
-    end
-
+  def empty
+    @arr_in.length == 0 && @arr_out.length == 0
+  end
 
 end
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue.new()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
