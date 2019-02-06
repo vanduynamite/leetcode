b3904f26d905fa0b92aaddda46217f73d24ba837
Paul van Duyn <paul.vanduyn@gmail.com>
2019-02-06 11:09:04 -0500

    1405 now SUPER FAST WOW

--- epi/1405_reconstruct_binary_tree.js
+++ epi/1405_reconstruct_binary_tree.js
@@ -6,16 +6,39 @@ class Node {
   }
 }
 
+// this one is O(n^2)
+// const reconstructBST = preorder => {
+//
+//   const helper = (left, right) => {
+//     if (right - left === 0) return null;
+//     let i = left + 1;
+//     while (preorder[i] < preorder[left]) i++;
+//     return new Node(preorder[left], helper(left + 1, i), helper(i, right));
+//   };
+//
+//   return helper(0, preorder.length);
+//
+// };
+
+
+// this one is O(n)!
 const reconstructBST = preorder => {
-  const helper = (left, right) => {
-    if (right - left === 0) return null;
-    let i = left + 1;
-    while (preorder[i] < preorder[left]) i++;
-    return new Node(preorder[left], helper(left + 1, i), helper(i, right));
+  let i = 0;
+
+  const helper = (left, max=null) => {
+    if (!preorder[left]) return null;
+    if (max && preorder[left] > max) return null;
+
+    i++;
+
+    const root = new Node(preorder[left]);
+    root.left = helper(left+1, root.value);
+    root.right = helper(i, max);
+
+    return root;
   };
 
-  return helper(0, preorder.length);
-
+  return helper(0);
 };
 
 
@@ -30,4 +53,7 @@ const reconstructBST = preorder => {
 
 // const inorder = [10, 20, 22, 25, 40, 60, 65, 70, 80];
 const preorder = [40, 20, 10, 25, 22, 60, 70, 65, 80];
-console.log(reconstructBST(preorder));
+const result = reconstructBST(preorder);
+console.log(result);
+console.log(result.left);
+console.log(result.right);
