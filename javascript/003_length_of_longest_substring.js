a165290c93be9d938d29a418878d4da6837c0a15
Paul van Duyn <paul.vanduyn@gmail.com>
2019-02-12 20:07:01 -0500

    finally LC003 is complete using a sliding window

---
+++ javascript/003_length_of_longest_substring.js
@@ -0,0 +1,30 @@
+
+
+var lengthOfLongestSubstring = function(s) {
+  let left = 0;
+  let best = 0;
+  let chars = new Set();
+
+  for (var i = 0; i < s.length; i++) {
+    if (chars.has(s[i])) {
+      while (chars.has(s[i])) {
+        chars.delete(s[left]);
+        left++;
+      }
+    } else if (i - left + 1 > best) {
+      // console.log(left, i);
+      best = i - left + 1;
+    }
+    chars.add(s[i]);
+
+  }
+
+  return best;
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
+console.log(lengthOfLongestSubstring('bbbbb'));
+console.log(lengthOfLongestSubstring('pwwkew'));
+console.log(lengthOfLongestSubstring('dvdf'));
+console.log(lengthOfLongestSubstring(''));
+console.log(lengthOfLongestSubstring('tmmzuxt'));
